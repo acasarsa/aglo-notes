262a7e3068ea4d18eb09f396fe04e1ff8d889a73
github username <github email address>
2020-10-17 16:02:49 -0400

    singly lists

--- data-structures/singly-linked-lists/Singly_Linked_List_Push.js
+++ data-structures/singly-linked-lists/Singly_Linked_List_Push.js
@@ -8,25 +8,40 @@ class Node{
 }
 
 class SinglyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
+  constructor(){
+    this.head = null
+    this.tail = null
+    this.length = 0
+    // this creates a sll head - tail - length
+  }
+  
 }
 
 var list = new SinglyLinkedList()
+list.push("Hello")
+list.push("Goodbye")
+console.log(list)
+
+// class SinglyLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.tail = null;
+//         this.length = 0;
+//     }
+//     push(val){
+//         var newNode = new Node(val);
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = this.head;
+//         } else {
+//             this.tail.next = newNode;
+//             this.tail = newNode;
+//         }
+//         this.length++;
+//         return this;
+//     }
+// }
+
+// var list = new SinglyLinkedList()
 // list.push("HELLO")
 // list.push("GOODBYE")
